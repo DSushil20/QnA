b1525543fdfe6a4e56ad812f7c7fa67e004f5c0d
Sushil Damdhere <sushildamdhere@mail.com>
2012-02-15 15:22:51 +0530

    Pushin initial app to GitHub

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QnA::Application.config.secret_token = '0fb6054a5143d3fc32f9d0e807e2920775a39cb0827a6cafd778ad7712d3d83c0838be4c7529cdf78e4eaa551da89c38fcf9ef48b9315ff0d65fe06918521329'
