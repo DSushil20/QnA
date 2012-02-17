8f3804ef78bb55177316d37ebea4038c4ec38413
Sushil Damdhere <sushildamdhere@mail.com>
2012-02-17 11:09:13 +0530

    Added users controller

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,7 +1,12 @@
 class LandingController < ApplicationController
 
 def start
-	
+
+	  @user = User.new
+      respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @user }
+	  end
 end
 
 end
