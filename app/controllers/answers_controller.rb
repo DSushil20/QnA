c743eafe3dc2b875c5e345641660db81a6b19ffc
Sushil Damdhere <sushildamdhere@mail.com>
2012-02-17 20:55:13 +0530

    Modified question controller and view

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,19 @@
 class AnswersController < ApplicationController
+
+	def create  	
+  	   @answer = Answer.new(params[:answer])
+
+    	respond_to do |format|
+     	 	if @answer.save
+        		format.html { #redirect_to landing_path 
+        		 }
+        		format.xml  { render :xml => @answer, :status => :created, :location => @answer }
+      		else
+        		format.html { render :action => "new" }
+        		format.xml  { render :xml => @answer.errors, :status => :unprocessable_entity }
+      		end
+    	end
+  	end
+
+
 end
