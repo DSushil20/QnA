d1a5ee060a517621a30b63924058a7f6d987cb7a
Sushil Damdhere <sushildamdhere@mail.com>
2012-02-20 16:53:53 +0530

    updated models to view references

---
+++ db/migrate/20120220104335_add_question_id_to_answer.rb
@@ -0,0 +1,11 @@
+class AddQuestionIdToAnswer < ActiveRecord::Migration
+  def self.up
+  	add_column :answers, :question_id, :integer
+  end
+
+
+  def self.down
+  	remove_column :answers, :question_id
+
+  end
+end
