51becd976683ccbb88501e380f7c4452af71d1e7
Sushil Damdhere <sushildamdhere@mail.com>
2012-02-23 20:31:24 +0530

    updated UserActivity model to update user karma

--- app/models/user_activity.rb
+++ app/models/user_activity.rb
@@ -4,5 +4,26 @@ class UserActivity < ActiveRecord::Base
 	belongs_to :answer
 	
 	attr_accessible :all
+
+	after_save :update_karma 
 	
+
+
+
+	private 
+	def update_karma 
+
+		user =  self.user
+
+		user.karma = 0.to_s if not user.karma
+
+		if self.vote == 1
+			user.karma  = (user.karma.to_i + 1).to_s
+		else 
+			user.karma  = (user.karma.to_i - 1).to_s
+		end
+
+		user.save
+	end		
+
 end
